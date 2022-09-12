6129d4cd9298714558c6391a963f217f621e8606
sy220902 <20211059@vision.hoseo.edu>
2022-09-13 04:09:40 +0900

    HelloSDL 실습
    HelloSDL 실습

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,6 @@ void render();
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-<<<<<<< Updated upstream
-=======
 	{
 		g_pWindow = SDL_CreateWindow("Setting up SDL",
 			SDL_WINDOWPOS_CENTERED,
@@ -56,33 +54,6 @@ int main(int argc, char* argv[])
 
 
 	return 0;
-
-	/*if (!init())
->>>>>>> Stashed changes
-	{
-		g_pWindow = SDL_CreateWindow("Setting up SDL",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 640, 480,
-			SDL_WINDOW_SHOWN);
-
-		if (g_pWindow != 0)
-		{
-			g_pRenderer
-				= SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-		else
-			return 1;
-	}
-
-	SDL_SetRenderDrawColor(
-		g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-
-	SDL_Delay(5000);
-	SDL_Quit();
-
-	return 0;*/
 }
 
 bool init()
