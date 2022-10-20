6ce632022531bfcb2f9c07f1d3bbb7db7c87b7d4
sy220902 <20211059@vision.hoseo.edu>
2022-10-20 22:00:28 +0900

    loadBMP
    loadBMP

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,127 @@
 ﻿#include "main.h"
 
 
+const int SCREEN_WIDTH = 1024; // 화면 가로 길이
+const int SCREEN_HEIGHT = 576; // 화면 세로 길이
 
-int main(int argc, char* argv[])
+// SDL을 초기화하고 윈도우를 생성한다
+bool init();
+
+// 미디어를 불러온다
+bool loadMedia();
+
+// 미디어를 해제하고 SDL을 종료한다
+void close();
+
+// 액자, Window (SDL_Window 구조체를 가리키는 포인터)
+SDL_Window* gWindow = NULL;
+
+// 그림, 윈도우에 포함될 Surface, 그래픽 메모리. Surface위에 그림을 그린다
+SDL_Surface* gScreenSurface = NULL;
+
+// 불러와서 화면에 출력할 이미지를 가리켜줄 변수
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
 {
-    SDL_Window* window = NULL; // 액자, Window (SDL_Window 구조체를 가리키는 포인터)
+    // 성공 / 실패 플래그 변수 만들기
+    bool success = true;
 
-    SDL_Surface* screenSurface = NULL; // 그림, 윈도우에 포함될 Surface, 그래픽 메모리. Surface위에 그림을 그린다
-
-    const int SCREEN_WIDTH = 1920; // 화면 가로 길이
-    const int SCREEN_HEIGHT = 1080; // 화면 세로 길이
-
-    // SDL 초기화(어떤 서브 시스템을 초기화할지 정하는 매개변수)
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) // 오류가 생기면 SDL_Init은 -1을 리턴한다
+    // SDL 초기화
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("SDL을 초기화할 수 없습니다! SDL_Error: %s\n", SDL_GetError()); // 에러를 콘솔창에 출력
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError()); // SDL 초기화 실패했을 경우 에러 메시지
+        success = false; // SDL 초기화에 실패했으면 플래그 변수로 표시해줌
     }
     else
     {
-        // 윈도우 만들기. 반환값은 SDL_Window 포인터. (윈도우의 이름(게임 이름), x 좌표,          y 좌표,                   가로 길이,     세로 길이,    생성flag(?))
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) // 에러가 생기면 SDL_CreateWindow는 NULL을 리턴한다.
+        // 윈도우 생성하기
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) // 윈도우 생성에 실패했다면 SDL_CreateWindow는 NULL을 반환한다
         {
-            printf("윈도우를 생성할 수 없습니다! SDL_Error: %s\n", SDL_GetError()); // 에러를 콘솔창에 출력
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError()); // 윈도우 생성 실패했을 경우 에러 메시지
+            success = false; // 윈도우 생성에 실패했으면 플래그 변수로 표시해줌
         }
         else
         {
-            // Surface와 Window를 연결하기. screenSurface가 window와 연결된 Surface가 되게 만든다
-            // 반환값은 SDL_Surface 포인터인데, Window와 연결된 Surface다.
-            // 이렇게 반환된 SDL_Surface의 경우, SDL_Window가 destroy되면 자동으로 같이 free()된다.
-            screenSurface = SDL_GetWindowSurface(window);
-
-            // surface를 하얗게 채운다(그림을 그릴 SDL_Surface 구조체의 포인터, 채울 직사각형을 표현하는 SDL_Rect 구조체의 포인터 또는 NULL(전체를 채움), 채울 색상)
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-        
-            // SDL_FillRect 함수를 이용해서 바뀐 Surface를 Window에 갱신시켜준다. (갱신할 Window의 포인터)
-            // 성공했다면 0, 실패했다면 음수를 반환한다.
-            SDL_UpdateWindowSurface(window);
-
-        
+            // gScreenSurface를 gWindow에 연결시켜줌
+            // 윈도우에 포함된 surface를 잡는다
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
         }
     }
 
-    // window의 메모리를 해제 (window가 해제되면 Surface도 같이 해제됨)
-    SDL_DestroyWindow(window);
+    return success; // 함수 init은 자신의 성공 여부 플래그 변수를 true / false로 반환
+}
 
-    // SDL 시스템의 종료
+bool loadMedia()
+{
+    // 성공 / 실패 플래그 변수 만들기
+    bool success = true;
+
+    gHelloWorld = SDL_LoadBMP("/Users/Admin/Documents/GitHub/gf2022-sy220902/assets/background.BMP");
+    if (gHelloWorld == NULL) // gHelloWorld에 제대로 이미지가 로드되지 않았는지 체크
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", "assets/background.BMP", SDL_GetError());
+        success = false;
+    }
+
+    return success; // 함수 loadMedia는 자신의 성공 여부 플래그 변수를 true / false로 반환
+}
+
+void close()
+{
+    // surface를 할당 해제시킨다(?)
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL; // 포인터 변수가 NULL을 가리키게 해주기
+
+    // 윈도우 삭제
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL; // 포인터 변수가 NULL을 가리키게 해주기
+
+    // SDL 서브시스템들을 종료시킨다
     SDL_Quit();
 
+}
 
+
+
+int main(int argc, char* argv[])
+{
+    // SDL을 시작하고 윈도우를 생성하기
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        // 미디어 불러오기
+        if (!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            // 이미지 적용하기
+            // SDL_BlitSurface를 이용해 로드된 surface를 스크린 surface 위에 blit한다.
+            // Blit은 비트들을 그래픽 메모리에서 다른 곳으로 복사하는 것을 뜻한다.
+            // (소스 이미지, ?, 이미지들의 목적지, ?)
+            // 백 버퍼로 렌더링 된다.
+            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+            // Surface를 업데이트한다.
+            // SDL_UpdateWindowSurface를 모든 blit이 끝날 때마다 호출할 필요 없이
+            // 현재 프레임의 모든 blit 작업이 끝난 후 호출하면 된다.
+            SDL_UpdateWindowSurface(gWindow);
+
+            // 3초를 지연시켜 윈도우가 바로 사라지지 않게 한다.
+            SDL_Delay(30000);
+        }
+    }
+
+    
+    
+    // 프로그램을 종료한다.
+    close();
+    
     return 0;
 }
