d334785c9580eac7cd4c0efe2f23a55aa67a4441
sy220902 <20211059@vision.hoseo.edu>
2022-10-26 05:02:18 +0900

    10260502
    10260502

--- include/Game.h
+++ include/Game.h
@@ -15,15 +15,39 @@ public:
 	bool running();
 	void handleEvents();
 	void clean();
-	bool move = false;
 
 private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	bool m_bRunning;
 
+
+
+	SDL_Event event;
+
+	enum LButtonSprite
+	{
+		BUTTON_SPRITE_MOUSE_OUT = 0,
+		BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
+		BUTTON_SPRITE_MOUSE_DOWN = 2,
+		BUTTON_SPRITE_MOUSE_UP = 3,
+		BUTTON_SPRITE_TOTAL = 4
+	};
+
+
+
+
 	//TextureManager m_textureManager;
-	int m_currentFrame;
+	int m_ground_grass;
+	int m_cursor;
+	int m_player_idle;
+	
+	int m_player_x = 10;
+	int m_player_y = 300;
+	int m_cursor_x = 200;
+	int m_cursor_y = 300;
+
+	
 
 
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -26,11 +26,28 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
         return false; // SDL 초기화 실패
     }
 
-
-    if (!TheTextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer))
+    // 배경 불러오기
+    if (!TheTextureManager::Instance()->load("Assets/ground_grass.png", "Ground_grass", m_pRenderer))
     {
         return false;
     }
+
+    // 커서 불러오기
+    if (!TheTextureManager::Instance()->load("Assets/cursor.png", "Cursor", m_pRenderer))
+    {
+        return false;
+    }
+
+
+    // 플레이어 불러오기
+    if (!TheTextureManager::Instance()->load("Assets/player_idle_animate.png", "Player", m_pRenderer))
+    {
+        return false;
+    }
+
+    
+
+
 
     m_bRunning = true;
     return true;
@@ -38,7 +55,10 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
 
 void Game::update()
 {
-    m_currentFrame = ((SDL_GetTicks() / 100) % 6);
+    
+        
+
+    //m_currentFrame = ((SDL_GetTicks() / 100) % 6);
     // 게임 데이터 갱신
 }
 
@@ -46,11 +66,25 @@ void Game::render()
 {
     SDL_RenderClear(m_pRenderer);
 
-    TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82,
-        m_pRenderer);
 
-    TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128,
-        82, 0, m_currentFrame, m_pRenderer);
+
+    //TheTextureManager::Instance()->draw("animate", 0, 0, 60, 90,
+    //    m_pRenderer);
+
+    // 배경 그리기
+    TheTextureManager::Instance()->draw("Ground_grass", 0, 0, 800,
+        600, m_pRenderer);
+
+    // 커서 그리기
+    TheTextureManager::Instance()->drawFrame("Cursor", m_cursor_x, m_cursor_y, 67,
+        70, 0, m_cursor, m_pRenderer);
+
+
+    // 플레이어 그리기
+    TheTextureManager::Instance()->drawFrame("Player", m_player_x, m_player_y, 60,
+        90, 0, m_player_idle, m_pRenderer);
+
+
 
     //m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
     //m_textureManager.drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
@@ -66,18 +100,48 @@ bool Game::running()
 
 void Game::handleEvents()
 {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) // if문은 한 번만 작동하고 끝나기 때문에 switch-case에서 뭐든지 하나의 케이스를 처리하면 끝나버리지만
+    
+
+
+    while (SDL_PollEvent(&event) != 0) // if문은 한 번만 작동하고 끝나기 때문에 switch-case에서 뭐든지 하나의 케이스를 처리하면 끝나버리지만
         // while문은 이벤트가 진행되는 동안 계속 작동되고 있기 때문에 QUIT이외에 다른 케이스도 더 추가할 수 있다. 
     {
-        switch (event.type)
+        if (event.type == SDL_QUIT)
         {
-        case SDL_QUIT:
             m_bRunning = false;
             break;
-        default:
-            break;
         }
+        else if (event.type == SDL_KEYDOWN)
+        {
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_UP:
+                m_player_y -= 3;
+                break;
+
+            case SDLK_DOWN:
+                m_player_y += 3;
+                break;
+
+            case SDLK_LEFT:
+                m_player_x -= 3;
+                break;
+
+            case SDLK_RIGHT:
+                m_player_x += 3;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        if (event.type == SDL_MOUSEMOTION || event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP)
+        {
+            SDL_GetMouseState(&m_cursor_x, &m_cursor_y);
+        }
+
+    
     }
 }
 
