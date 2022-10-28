edb0df7e218dd215f73469f2afb7e2bf80a17274
sy220902 <112677703+sy220902@users.noreply.github.com>
2022-10-28 13:53:09 +0900

    상속 실습1, 실습2
    상속 실습1, 실습2

--- include/Game.h
+++ include/Game.h
@@ -1,7 +1,8 @@
 #ifndef __Game__
 #define __Game__
 #include <TextureManager.h>
-
+#include <GameObject.h>
+#include <Player.h>
 
 class Game
 {
@@ -21,6 +22,8 @@ private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	bool m_bRunning;
+	GameObject m_go;
+	Player m_player;
 
 	//TextureManager m_textureManager;
 	int m_currentFrame;
--- src/Game.cpp
+++ src/Game.cpp
@@ -27,10 +27,16 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     }
 
 
+
     if (!TheTextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer))
     {
         return false;
     }
+
+
+    m_go.load(100, 100, 128, 82, "animate");
+    m_player.load(300, 300, 128, 82, "animate");
+
 
     m_bRunning = true;
     return true;
@@ -38,7 +44,11 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
 
 void Game::update()
 {
-    m_currentFrame = ((SDL_GetTicks() / 100) % 6);
+    {
+        m_go.update();
+        m_player.update();
+    }
+    // m_currentFrame = ((SDL_GetTicks() / 100) % 6);
     // 게임 데이터 갱신
 }
 
@@ -46,11 +56,17 @@ void Game::render()
 {
     SDL_RenderClear(m_pRenderer);
 
+    m_go.draw(m_pRenderer);
+    m_player.draw(m_pRenderer);
+
+    
+    /*
     TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82,
         m_pRenderer);
 
     TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128,
         82, 0, m_currentFrame, m_pRenderer);
+        */
 
     //m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
     //m_textureManager.drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
